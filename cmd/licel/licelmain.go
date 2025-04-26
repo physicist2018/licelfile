e42d166f63675d578194da46b788b82088e5cf49
physicist2018 <physicist2018@yandex.ru>
2025-04-27 05:43:07 +1000

    Initial commit

---
+++ cmd/licel/licelmain.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/physicist2018/licelfile/licelformat"
+)
+
+func main() {
+
+	a := licelformat.NewLicelPack("b*.*")
+	v := licelformat.SelectCertainWavelength2(&a, true, 408)
+	fmt.Println(v)
+	for key := range a {
+		fmt.Println(key)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/physicist2018/licelfile
+
+go 1.13
+
+require github.com/archsh/timefmt v0.10.1
