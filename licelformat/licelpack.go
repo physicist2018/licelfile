bf4bec68bb06142bff19e49d75534d17951427ad
physicist2018 <physicist2018@yandex.ru>
2025-05-12 04:01:49 +1000

    Add new functionality

--- licelformat/licelpack.go
+++ licelformat/licelpack.go
@@ -2,22 +2,32 @@ package licelformat
 
 import (
 	"path/filepath"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
 
-type LicelPack map[string]LicelFile
+type LicelPack struct {
+	ID        string               `bson:"_id,omitempty"`
+	StartTime time.Time            `bson:"start_time"`
+	Data      map[string]LicelFile `bson:"data"`
+}
 
 // NewLicelPack — loads files according to mask
-func NewLicelPack(mask string) LicelPack {
-	pack := make(LicelPack)
+func NewLicelPack(mask string) *LicelPack {
+	pack := &LicelPack{
+		Data: make(map[string]LicelFile),
+	}
 	files, err := filepath.Glob(mask)
 	if err != nil {
 		log.Fatal().Err(err).Str("mask", mask).Msg("Error getting files by mask")
 	}
-	for _, fname := range files {
-		pack[fname] = LoadLicelFile(fname)
-		println(pack[fname].NDatasets)
+	for i, fname := range files {
+		pack.Data[fname] = LoadLicelFile(fname)
+		if i == 0 {
+			pack.StartTime = pack.Data[fname].MeasurementStartTime
+		}
+
 		//println(pack[fname].)
 
 	}
@@ -27,7 +37,7 @@ func NewLicelPack(mask string) LicelPack {
 // SelectCertainWavelength2 — selects certain profile by its wavelength and type from a LicelPack
 func (lp *LicelPack) SelectCertainWavelength(isPhoton bool, wavelength float64) LicelProfilesList {
 	var result LicelProfilesList
-	for _, file := range *lp {
+	for _, file := range lp.Data {
 		profile := file.SelectCertainWavelength(isPhoton, wavelength)
 		if profile.Wavelength != 0 {
 			result = append(result, profile)
@@ -38,7 +48,7 @@ func (lp *LicelPack) SelectCertainWavelength(isPhoton bool, wavelength float64) 
 
 // Save - saves LicelPack to files
 func (lp *LicelPack) Save() error {
-	for fname, licf := range *lp {
+	for fname, licf := range lp.Data {
 		if err := licf.Save(fname); err != nil {
 			return err
 		}
