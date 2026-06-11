45acf4b7f978a3493756c43ae319f1f024aa5b60
physicist2018 <physicist2018@yandex.ru>
2026-06-11 18:02:49 +1000

    update main code

--- cmd/licel/licelmain.go
+++ cmd/licel/licelmain.go
@@ -8,13 +8,20 @@ import (
 )
 
 func main() {
-	a, err := licelformat.LoadLicelFile("a16A0421.070296")
+	a, err := licelformat.NewLicelPackFromZip("archive.zip")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading file: %v\n", err)
 		os.Exit(1)
 	}
-	for i := range a.Profiles {
-		println(a.Profiles[i].Wavelength, a.Profiles[i].Photon, a.Profiles[i].NShots, a.Profiles[i].DiscrLevel)
-		fmt.Printf("%.2f %.2f %.2f\n", a.Profiles[i].Data[0], a.Profiles[i].Data[1], a.Profiles[i].Data[2])
+	err = a.SaveToNetCDF3("1.nc")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error saving file: %v\n", err)
+		os.Exit(1)
 	}
+	b, err := licelformat.LoadLicelPackFromNetCDF3("1.nc")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading file: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(b.StartTime)
 }
